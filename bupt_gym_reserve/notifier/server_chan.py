1a365ded497afd44498ea0070d738b9588bd81c8
Hyiker <961531608@qq.com>
2021-03-21 22:10:09 +0800

    serverchan fix

--- bupt_gym_reserve/notifier/server_chan.py
+++ bupt_gym_reserve/notifier/server_chan.py
@@ -10,7 +10,7 @@ class SeverChanNotifier:
     def __init__(self, sckey) -> None:
         self.config = {
             'sckey': sckey,
-            'url': 'https://sc.ftqq.com/SCU129706T96bb814b7a12079eec2691ea2cd563055fbdf1566ba58.send'
+            'url': 'https://sc.ftqq.com/'
         }
 
     def send_msg(self, title: str, content: str) -> tuple:
@@ -18,7 +18,7 @@ class SeverChanNotifier:
             'text': title,
             'desp': content
         }
-        resp = requests.post(url=self.config['url'], data=payload)
+        resp = requests.post(url=f"{self.config['url']}{self.config['sckey']}.send", data=payload)
         if resp.status_code != 200:
             return (False, '请求服务器出错')
         json_resp = json.loads(resp.text)
